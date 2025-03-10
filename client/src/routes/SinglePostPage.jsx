daafa1102e0b69ce819fb87d6c9b92b056136392
olayemi <olayemio521@gmail.com>
2025-03-10 12:39:52 -0400

    commit

--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -39,7 +39,7 @@ const SinglePostPage = () => {
           </h1>
           <div className="flex items-center gap-2 text-gray-400 text-sm">
             <span>Written by</span>
-            <Link className="text-blue-800">CyberTailor</Link>
+            <Link className="text-blue-900">CyberTailor</Link>
             <span>on</span>
             <Link className="text-blue-800">{data.category}</Link>
             <span>{format(data.createdAt)}</span>
